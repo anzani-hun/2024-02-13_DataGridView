15f1eabbe076e057e4460232beb2e0cd6b11422c
anzani-hun <anzani@hun>
2024-02-13 19:00:30 +0100

    001-eddig_jutottunk

--- 2024-02-13_DataGridView/Form1.cs
+++ 2024-02-13_DataGridView/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MySql.Data.MySqlClient;
 
 namespace _2024_02_13_DataGridView
 {
@@ -15,6 +16,56 @@ namespace _2024_02_13_DataGridView
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            tablazatBeallitasai();
+            tablazatFrissitese();
+        }
+
+        private void tablazatFrissitese()
+        {
+            //throw new NotImplementedException();
+        }
+
+        private void tablazatBeallitasai()
+        {
+            //-- egész táblázatra ható beállítás
+            dataGridView_Tabla.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            
+            //-- fejléc beállításai
+            dataGridView_Tabla.ColumnHeadersDefaultCellStyle.BackColor = Color.Black;
+            dataGridView_Tabla.ColumnHeadersDefaultCellStyle.ForeColor = Color.White;
+            dataGridView_Tabla.ColumnHeadersDefaultCellStyle.Font = new Font(this.Font, FontStyle.Italic);
+
+            //-- oszlopok létrehozása, beállítása és tulajdonságai
+            DataGridViewColumn column_orszag = new DataGridViewColumn();
+            {
+                column_orszag.Name = "orszag";
+                column_orszag.HeaderText = "Ország neve";
+                column_orszag.CellTemplate = new DataGridViewTextBoxCell();
+            }
+
+            dataGridView_Tabla.Columns.Add(column_orszag);
+            //-- fovaros ----------------------------------
+            DataGridViewColumn column_fovaros = new DataGridViewColumn();
+            {
+                column_fovaros.Name = "fovaros";
+                column_fovaros.HeaderText = "Főváros";
+                column_fovaros.CellTemplate = new DataGridViewTextBoxCell();
+            }
+            dataGridView_Tabla.Columns.Add(column_fovaros);
+            //-- terulet ----------------------------------
+            DataGridViewColumn column_terulet = new DataGridViewColumn();
+            {
+                column_terulet.Name = "terulet";
+                column_terulet.HeaderText = "Terület";
+                column_terulet.CellTemplate = new DataGridViewTextBoxCell();
+            }
+            dataGridView_Tabla.Columns.Add(column_terulet);
+
+
         }
     }
 }
